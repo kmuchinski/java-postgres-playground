dabdf05281c7c6efb24e3f8779fda605a77258e4
Kleisson Muchinski <kmuchinski@gmail.com>
2023-07-27 22:20:27 +0000

    Implementado Array

---
+++ src/main/java/com/example/AppArray.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.util.Arrays;
+
+public class AppArray {
+    public static void main(String[] args) {
+        int vetor[] = {10, 11, 67};
+        System.out.println("O vetor: " + Arrays.toString(vetor));
+
+        System.out.println("-------------------------");
+    }
+}
