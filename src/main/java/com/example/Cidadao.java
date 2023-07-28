c310758b87f9d2226dfbb2a1071224ff1a1c81dd
Kleisson Muchinski <kmuchinski@gmail.com>
2023-07-28 20:10:20 +0000

    Inclusão classe eleitor

---
+++ src/main/java/com/example/Cidadao.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.time.LocalDate;
+
+public class Cidadao {
+    private LocalDate dataNascimento;
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int idade(){
+        return LocalDate.now().getYear() - dataNascimento.getYear();
+    }
+
+    public String eleitor(){
+        int idade = idade();
+        if(idade < 16)
+            return "Não é Eleitor";
+        if(idade > 15 && idade < 18 || idade > 70)
+            return "Eleitor facultativo";
+        else
+            return "Eleitor Obrigatório";
+    }
+}
