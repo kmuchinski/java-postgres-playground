10e9d189078174085ec2f5d6d4cb6a9fe0e2d2f9
Kleisson Muchinski <kmuchinski@gmail.com>
2023-07-29 18:46:41 +0000

    Sofisticando Arrays.

--- src/main/java/com/example/AppArray.java
+++ src/main/java/com/example/AppArray.java
@@ -5,6 +5,13 @@ import java.util.Arrays;
 public class AppArray {
     public static void main(String[] args) {
         int vetor[] = {10, 20, 30};
+
+        System.out.println("----------------------------------");
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = 100 * (i+1);
+            
+        }
+        System.out.println("----------------------------------");
         
         System.out.println("Valor da Posição 0: " + vetor[0]);
         System.out.println("Valor da Posição 1: " + vetor[1]);
@@ -22,5 +29,21 @@ public class AppArray {
         //Impressão de Matriz
         System.out.println(Arrays.toString(matriz[0]));
         System.out.println(Arrays.toString(matriz[1]));
+
+        System.out.println("--------------***--------------------");
+
+        double matriz2[][] = new double[2][3];
+        for (int i = 0; i < matriz2.length; i++) {
+            for (int j = 0; j < matriz2[i].length; j++) {
+                matriz2[i][j] = i * matriz2[i].length + j+1;
+            }
+        }
+        //Imprimir a matriz
+        for (int i = 0; i < matriz2.length; i++) {
+            for (int j = 0; j < matriz2[i].length; j++) {
+                System.out.printf("%5.1f ",matriz2[i][j]);
+            }
+            System.err.println();
+        }
     }
 }
